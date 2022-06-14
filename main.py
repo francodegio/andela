9579f4b8632960e5119512001c667e404889bc96
Franco De Giovannini <franco.degiovannini@kavak.com>
2022-06-14 15:01:14 -0300

    remove logs

--- main.py
+++ main.py
@@ -10,17 +10,14 @@ from andela.definitions import PriceModel, TripPayload
 
 app = FastAPI()
 logger = logging.getLogger()
-try:
-    path = 'models/price_model.pkl'
-    model = PriceModel(path)
-except:
-    logger.error(traceback.format_exc())
+path = 'models/price_model.pkl'
+model = PriceModel(path)
+
 
 @app.post('/')
 async def predict(data: TripPayload):
     try:
         logger.info("I'm here!")
-        #data = TripPayload(**data)
         df = pd.DataFrame([data.__dict__])
         df = model.transform(df)
         return {"prediction": str(model.predict(df)[0])}
