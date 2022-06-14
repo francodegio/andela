2e170de2976088733f7f9b446d0059d7dee52d88
Franco De Giovannini <franco.degiovannini@kavak.com>
2022-06-14 17:55:10 -0300

    bugfix library import

--- andela/train.py
+++ andela/train.py
@@ -9,7 +9,6 @@ from typing import Tuple
 from math import sqrt
 
 from catboost import CatBoostRegressor
-from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import OneHotEncoder
@@ -19,40 +18,6 @@ from sklearn.metrics import (
     mean_absolute_percentage_error,
     r2_score
 )
-
-
-
-class DataFrameSelect(BaseEstimator, TransformerMixin):
-    
-    def __init__(self, column_names):
-        self.column_names = column_names
-    
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        return X[self.column_names]
-    
-class DateTimeHandler(BaseEstimator, TransformerMixin):
-    
-    def __init__(self):
-        pass
-    
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, x):
-        date_serie = pd.to_datetime(x)
-        features = []
-        features.append(date_serie.dt.month)
-        features.append(date_serie.dt.day)
-        features.append(date_serie.dt.weekday)
-        features.append(date_serie.dt.hour)
-        features.append(date_serie.dt.minute)
-        df = pd.concat(features, axis=1)
-        df.columns = ['month', 'day', 'weekday', 'hour', 'minute']
-
-        return df
 
 
 def preprocess():
@@ -167,6 +132,9 @@ def make_markdown_report(metrics):
 
 
 if __name__ == '__main__':
+
+    from andela.definitions import DataFrameSelect, DateTimeHandler
+
     if not os.path.exists('../models/price_model.pkl'):
         os.makedirs('../models', exist_ok=True)
         
